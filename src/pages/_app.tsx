81f705d2b2676be302e69ead38abe205d0c2078e
Vitor Markis <vitormarkis2369@gmail.com>
2023-09-08 14:21:10 -0300

    getting started

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ThemeProvider } from "next-themes"
+import type { AppProps } from "next/app"
+import "@/styles/globals.css"
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="dark"
+    >
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
